6f87720dc75ae53b4d92fe0e22025e2216269a29
Tristan Roberts <tristanerr@gmail.com>
2020-05-30 04:33:12 -0500

    Restylized the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,111 +11,56 @@ const Home = () => {
         <div className='homepage'>
             <Container>
             {/*this div represents the first element (hero) of the landing page*/}
-                <div className='homepage-hero'>
-                    <Row>
-                        <Col className='homepage-hero-c1'>
-                            <h1>the web3 </h1>
-                            <p>scientific</p>
-                            <p>society</p>
-                            <a target="_blank" rel="noopener noreferrer"  href="https://luisivan.net/posts/expert-daos"><Button className='contact-us-btn'>What is an Expert DAO?</Button></a>
-                        </Col>
-                        <Col className='homepage-hero-c2'>
-                            <p className='about-rs'>Develop your expertise. </p>
-                            <p> Earn currency and kudos. </p>
-                            <p className='about-rs'>Expert DAO members curate content and arbitrate decisions in a swarm of domains.</p>
-                            <span className='call-to-action'>
-                                <a href="https://tristanerr.typeform.com/to/VOEZ4F"><Button mode="strong" className='call-to-action-btn'>Apply to be a Curator</Button></a>
-                            </span>
+                    <Row  className='homepage-hero'>
+                        <Col lg={4}>
+                        <Card>
+                            <Card.Body>
+                                <Card.Text>
+                                <h1 className="centerText hero-title">Logo will go here</h1>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                      </Col>
+                        <Col lg={8} className="margins" >
+                             <h1 className="centerText hero-sub-title pushUp">a foundation for the scientific Web-of-Trust</h1>
+                             <h1 className="centerText hero-title">Research Collective</h1>
+                             <h1 className="centerText hero-sub-title pushUp">a moderately friendly network of Expert DAOs</h1>
+                             <div className="buttonContainer flexContainer">
+                               <a  className="paddingRight" target="_blank" rel="noopener noreferrer"  href="https://luisivan.net/posts/expert-daos"><Button label="What is an Expert DAO?" mode="neutral"/></a>
+
+                                <a className="paddingLeft" target="_blank" rel="noopener noreferrer"  href="https://tristanerr.typeform.com/to/VOEZ4F"><Button className="rc-button" label="Join an Expert DAO" /></a>
+
+                             </div>
                         </Col>
                     </Row>
-                </div>
             </Container>
-
-            {/*this section is for "Why Research Collective"*/}
-            <Container>
-                <div className='homepage-why'>
-                    <Row>
-                        {/*Resilient*/}
-                        <Col>
-                            <Card>
-                                <Card.Img className='glyph' variant='top' src='homepage-glyph/resilient.png' />
-                                <Card.Body>
-                                    <Card.Title className='why-rs-title'>Resilient</Card.Title>
-                                    <Card.Text className='why-rs-body'>
-                                        Research Collective is an
-                                        evolving social, legal, and ethereal
-                                        fosters coordination between
-                                        DAOs, humans, and legal entities
-                                        to accomplish feats that may
-                                        seem impossible.
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        {/*Immutable*/}
-                        <Col>
-                            <Card>
-                                <Card.Img className='glyph' variant='top' src='/homepage-glyph/immutable.png' />
-                                <Card.Body>
-                                    <Card.Title className='why-rs-title'>Immutable</Card.Title>
-                                    <Card.Text className='why-rs-body'>
-                                        Distributed ledgers are
-                                        used to timestamp
-                                        evidence, streamlining intellectual property claims and preventing researchers from changing their
-                                        hypotheses to fit their experiment’s data.
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        {/*Open*/}
-                        <Col>
-                            <Card>
-                                <Card.Img className='glyph' variant='top' src='/homepage-glyph/open.png'/>
-                                <Card.Body>
-                                    <Card.Title className='why-rs-title'>Distributed</Card.Title>
-                                    <Card.Text className='why-rs-body'>
-                                        We are a global network
-                                        of researchers working towards
-                                        open-source biotechnology and its
-                                        availability to the masses,
-                                        as rapidly and ethically possible.
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
-                </div>
-            </Container>
-
 
             {/*this section is for 'Resources'*/}
             <Container>
                 <div className='resources-section'>
-                    <p className='section-header'>Resources</p>
+                    <p className='section-header'>Covid19 Resources</p>
                         <p className='homepage-resources-text'>
-                            It is a long established fact that a reader will be distracted by the readable content
-                            of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
-                            more-or-less normal distribution of letters
+                            curated lists of vendors, articles, and registries with label 'covid'
                         </p>
                         <Link to="/resources">
                         <Image src='homepage-glyph/resources-image.png' fluid={true}/>
-                        <Button mode="strong" className='resources-link'>See All Resources</Button>
+                        <Button mode="strong" className='rc-button resources-link'>Browse All Resources</Button>
                         </Link>
                 </div>
             </Container>
 
             {/*this is how it works section*/}
             <Container>
-                <div className='hiw-section'>
-                    <p className='section-header'>Decentralized Science</p>
+                <div>
+                    <p className="section-header sectionBreak">Method</p>
                     <Row>
                         <Col>
                             <Card className='hiw-card'>
                                 <Card.Img className='hiw-glyph' variant='top' src='homepage-glyph/propose.png' />
                                 <Card.Body>
-                                    <Card.Title className='hiw-title'>Propose</Card.Title>
+                                    <Card.Title className='hiw-title'>Curate</Card.Title>
                                     <Card.Text className='hiw-body'>
-                                        <p>Researchers identify the open questions that your professors wouldn't talk about.</p> <br/>
+                                        <p>Researchers identify the open questions that academics don't want to admit exist.</p> <br/>
                                         <p>A knowledge market is instantiated, allowing bets for and against the hypothesis. </p>
                                     </Card.Text>
                                 </Card.Body>
@@ -140,7 +85,7 @@ const Home = () => {
                                     <Card.Title className='hiw-title'>Review</Card.Title>
                                     <Card.Text className='hiw-body'>
                                         <p>If the data is conclusive, the Collective takes a position on the knowledge market...</p> <br/>
-                                        <p>...just moments before making the experiment's data available publicly.</p>
+                                        <p>...just moments before making the experiment's data available to the public.</p>
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
@@ -151,8 +96,8 @@ const Home = () => {
                                 <Card.Body>
                                     <Card.Title className='hiw-title'>Develop</Card.Title>
                                     <Card.Text className='hiw-body'>
-                                    <p>The winnings from the knowledge market are used to initiate development via <a href="https://www.molecule.to">Molecule.</a> </p><br/>
-                                    <p>A tokenized bonded curve allows researchers, institutions, and corporations to align incentives to bring the idea to market.</p>
+                                    <p>The winnings from the knowledge market are used to fund clinical development via <a href="https://www.molecule.to">Molecule.</a> </p><br/>
+                                    <p>A token allows researchers and institutions to align incentives to bring the idea to market.</p>
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
@@ -161,29 +106,100 @@ const Home = () => {
                 </div>
             </Container>
 
-            {/* this is contact form section*/}
+            {/*this section is for "Why Research Collective"*/}
             <Container>
+                <div className='homepage-why'>
+                   <p className='sectionBreak section-header'>Principles</p>
+                    <Row>
+                        {/*Resilient*/}
+                        <Col>
+                            <Card className="CardB">
+                                <Card.Img className='glyph' variant='top' src='homepage-glyph/resilient.png' />
+                                <Card.Body>
+                                    <Card.Title className='why-rs-title'>Anti-Fragile</Card.Title>
+                                    <Card.Text className='why-rs-body'>
+                                        With luck and planning, stress becomes a signal for growth.<br/> <br/>
+                                        An ever shifting aggregation of
+                                        social, legal, and ethereal organizations
+                                        can accomplish otherwise impossible tasks.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        {/*Immutable*/}
+                        <Col>
+                            <Card className="CardB">
+                                <Card.Img className='glyph' variant='top' src='/homepage-glyph/immutable.png' />
+                                <Card.Body>
+                                    <Card.Title className='why-rs-title'>Immutable</Card.Title>
+                                    <Card.Text className='why-rs-body'>
+                                        What is made public, for better or worse, should inform the future.<br/><br/>Distributed ledgers can be used to timestamp evidence, make intellectual property claims, and call out misconduct.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        {/*Open*/}
+                        <Col>
+                            <Card className="CardB">
+                                <Card.Img className='glyph' variant='top' src='/homepage-glyph/open.png'/>
+                                <Card.Body>
+                                    <Card.Title className='why-rs-title'>Distributed</Card.Title>
+                                    <Card.Text className='why-rs-body'>
+                                        It is possible to be in all places at once, yet hostage to none.
+                                        <br/><br/>
+                                        Anonymous, revokable credentials tied to decentralized identifiers enable self-sovereign data collection and publishing.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                  </div>
+            </Container>
+
+            {/*}
+            <Container>
+              <div>
+                <p className='about-rs'>Develop your expertise. </p>
+                <p> Earn currency and kudos. </p>
+                <p className='about-rs'>Expert DAO members curate content and arbitrate decisions in a swarm of domains.</p>
+                <span className='call-to-action'>
+                    <a href="https://tristanerr.typeform.com/to/VOEZ4F"><Button mode="strong" className='call-to-action-btn'>Apply to be a Curator</Button></a>
+                </span>
+
+              </div>
+            </Container> */}
+
+
+            {/* this is contact form section*/}
+            {/*// email should go to social@researchcollective.io*/}
+            <Container>
+              <Row>
+                <Col lg={3}/>
+                <Col lg={6}>
                 <div className='contact-us'>
-                    <p className='section-header'>Contact Us</p>
+                    <p className='section-header sectionBreak'>Contact Us</p>
                         <InputGroup className='contact-form-input mb-3'>
                             <FormControl
-                                placeholder='Enter Your Name'
+                                placeholder='Name'
                                 aria-label='Enter Your Name'
                                 aria-describedby='basic-addon2'
                             />
                         </InputGroup>
                         <InputGroup className='contact-form-input mb-3'>
                             <FormControl
-                                placeholder='Enter Your Email'
+                                placeholder='Email'
                                 aria-label='Enter Your Email'
                                 aria-describedby='basic-addon2'
                             />
                         </InputGroup>
                         <Form.Group className='contact-form-input'>
-                            <Form.Control as='textarea' rows='5' placeholder='Enter Your Message'/>
+                            <Form.Control as='textarea' rows='5' placeholder='What is on your mind?'/>
                         </Form.Group>
-                        <Button mode="strong" className='contact-send-btn'>Send</Button>
+                        <Button mode="strong" className='rc-button contact-send-btn'>Send</Button>
                 </div>
+                </Col>
+                  <Col lg={3}/>
+                </Row>
             </Container>
         </div>
     )
