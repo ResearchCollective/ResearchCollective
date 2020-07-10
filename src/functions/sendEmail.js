174bb6528eaa7eac33e350522dae0de93c4ed22f
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-07-10 14:14:43 -0400

    removed sendEmail's nodemailer logic as moved to mailing service

--- src/functions/sendEmail.js
+++ src/functions/sendEmail.js
@@ -1,35 +1,7 @@
 const sendEmail = async (formData) => {
-    // extracting data//
-    let {fullName, email, message} = formData
-    console.log('This is from sendEmail',formData)
-    // Note to self:- Do not push code with SMTP config, put them in enviroment variable//
-    // let transporter = nodemailer.createTransport({
-    //     host: '',//transporter host name
-    //     port: 587,//transporter port
-    //     secure: false, //true for port 465, false for other ports
-    //     auth: {
-    //         user: 'transporterAccount.email', // transporter email
-    //         pass: 'transporterAccount.pass', // transporter account password
-    //       },
-    //     //this is just for testing on localhost //
-    //       tls: {
-    //         rejectUnAuthorized: false
-    //       }
-    // })
-    // // send mail with transporter//
-    // let mail = await transporter.sendMail({
-    //     from: '"Research Collective Contact" <transporterAccount.email>', 
-    //     to: 'social@researchcollective.io', // can put multiple accounts//
-    //     subject: 'Research Colletive Contact Request',
-    //     text: '', //plain text body//
-    //     html: '' //object from user's response 
-    // })
-    // // Message sent//
-    // console.log("Message sent: %s", mail.messageId);
-    // // Preview only available when sending through an Ethereal account
-    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(mail));
 }
-//for catching errors//
-sendEmail().catch(console.error);
+
+        
+
 
 export default sendEmail
