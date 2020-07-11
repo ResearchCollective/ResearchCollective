4d49f4011516ae423275a52d0fd0b09847955f80
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-07-11 13:12:11 -0400

    temp commit for checkout

--- src/functions/sendEmail.js
+++ src/functions/sendEmail.js
@@ -1,4 +1,17 @@
+import axios from 'axios';
+
 const sendEmail = async (formData) => {
+  // get data from user input to send post request//
+  // const {fullName, email, message} = formData
+  // make http request to gcp serverless micro-service with axios//
+  axios.post('', formData)
+  .then((response) => {
+    console.log('This is response from rs-mailing-serivice', response )
+  })
+  .catch((error) => {
+    console.log('Error from rs-mailing-service', error)
+  })
+  // check response for user feedback//
 }
 
         
